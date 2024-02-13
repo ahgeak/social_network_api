7c6da0c0d3e27f5936443a21eb1f155471dd39a5
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-02-13 15:36:00 +0700

    created the thought model with vitual for reactionCount

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create the thought model
+const thoughSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+    },
+);
+
+// Virtual property `reactionCount` that retrieves the length of the thought's reactions array field on query.
+thoughSchema.virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughSchema);
+
+module.exports = Thought;
